dc8bb3ae31860c0975868c4d7426c1942f74483e
kafene <kafene@users.noreply.github.com>
2019-01-08 17:09:03 -0500

    wrap in try/finally to ensure file resource is closed

--- str_putcsv.php
+++ str_putcsv.php
@@ -11,20 +11,24 @@ if (!function_exists('str_putcsv'))
         // Open an in-memory file resource
         $fp = fopen('php://temp', 'r+b');
 
-        // Write the fields array to the file resource as a CSV line
-        fputcsv($fp, $fields, $delimiter, $enclosure, $escape);
+        try {
+            // Write the fields array to the file resource as a CSV line
+            fputcsv($fp, $fields, $delimiter, $enclosure, $escape);
 
-        // Rewind the file resource so it can be read
-        rewind($fp);
+            // Rewind the file resource so it can be read
+            rewind($fp);
 
-        // Read the entire CSV line
-        $csv = stream_get_contents($fp);
+            // Read the entire CSV line
+            $csv = stream_get_contents($fp);
 
-        // Remove the trailing line feed added by fputcsv
-        $csv = rtrim($csv, "\n");
-
-        // Close the file resource
-        fclose($fp);
+            // Remove the trailing line feed added by fputcsv
+            $csv = rtrim($csv, "\n");
+        } finally {
+            // Close the file resource
+            if (is_resource($fp)) {
+                fclose($fp);
+            }
+        }
 
         return $csv;
     }
